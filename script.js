23a13dceeec4d13bd9a532c0153e7953b5e161f2
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-09-02 22:21:59 -0300

    Created Scroll to Top button

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+function topo(){
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
 window.addEventListener("scroll", function(){  
   var header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 0);
+  var btn = document.querySelector("button");
+  header.classList.toggle("sticky", window.scrollY > 200);
+  btn.classList.toggle("show", window.scrollY > 200);   
 })
